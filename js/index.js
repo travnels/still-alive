a9fd50c9341beba32ebd9b18b267c4e7a62b6f92
Mike Szwedo <mike.szwedo@gmail.com>
2012-09-19 20:53:09 -0600

    Still Alive html and js and the jquery mobile library

---
+++ js/index.js
@@ -0,0 +1,22 @@
+
+
+$("#upButton").live("click",function() {
+    alert(' Up Button has been clicked');
+})
+
+$("#downButton").live("click",function() {
+    alert(' Down Button has been clicked');
+})
+
+$("#leftButton").live("click",function() {
+    alert(' Left Button has been clicked');
+})
+
+$("#rightButton").live("click",function() {
+    alert(' Right Button has been clicked');
+})
+
+$("#fireButton").live("click",function() {
+    alert(' Fire Button has been clicked');
+})
+
